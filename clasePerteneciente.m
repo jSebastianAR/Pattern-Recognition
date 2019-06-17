ccbe7e5fcfa70f59fb7bf2da0fde20e384369287
Sebastián Acosta Rosales <jSebastian_AR@outlook.com>
2019-06-16 23:53:32 -0500

    All files were deleted because were been in the wrong directory

--- clasePerteneciente.m
+++
@@ -1,7 +0,0 @@
-%Funcion que define la clase a la que pertenece el vector a clasificar
-
-function [numClase] = clasePerteneciente(distanciasM) %Recibe el arreglo que guarda todas las distancias de las medias de las clases y el vector dado por el usuario
- distanciaMinima = min(distanciasM);%encuentra la distancia más corta
- numClase = find(distanciasM==distanciaMinima); %encuentra la posicion donde se ubica la distancia minima en el arreglo de distancias
-end
-
