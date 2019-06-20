39a50ed5d175f233def72da08e40e83c874aafd9
Sebastián Acosta Rosales <jSebastian_AR@outlook.com>
2019-06-20 15:20:11 -0500

    Archivos .m agregados a carpeta Practice 8

---
+++ Practice 8/calculateDistance.m
@@ -0,0 +1,22 @@
+function [distances,numGroup] = calculateDistance(GROUPS,sample)
+import classGroup
+%{
+INPUTS
+GROUPS = instances of classGroup wich contains the samples and the mean(Z) value
+sample = current sample to classify
+
+OUTPUTS
+distances = array with all the distances between the current sample and all the means
+numGroup = number of group wich the sample belongs
+%}
+    distances = [];
+    numGroups = length(GROUPS);
+    for i=1:numGroups
+        distance = norm(GROUPS(i).Z-sample);
+        distances = [distances distance];
+    end
+    
+    dmin = min(distances);%find the group who belongs the sample
+    Group = find(distances==dmin);
+    numGroup = Group(1);
+end
